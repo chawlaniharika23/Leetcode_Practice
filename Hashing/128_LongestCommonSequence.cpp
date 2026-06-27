cc82c945ed7253655bb812ce1e74b8e11a2e7a35
Niharika Chawla <chawlaniharika23@gmail.com>
2026-06-27 15:37:59 +0530

    Update 128_LongestCommonSequence.cpp

--- Hashing/128_LongestCommonSequence.cpp
+++ Hashing/128_LongestCommonSequence.cpp
@@ -21,3 +21,7 @@ public:
         return longest;
     }
 };
+
+/*Approach : Traverse the elements of array one at a time, store them in a set
+if you find num-1, then consecutive sequence does not start from current element, skip
+if n-1 doesnt exist, len=1; look for n+1, increase length until you cannot find n+1*/
