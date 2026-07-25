b569c1c9489cccfc8a95568d29b1c6a59242bd77
Niharika Chawla <chawlaniharika23@gmail.com>
2026-07-25 23:08:14 +0530

    Create 560_SubarraySumEqualsK.cpp

---
+++ Arrays/560_SubarraySumEqualsK.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map <int,int> mpp;
+        int presum=0, count=0;
+        mpp[0]=1;
+
+        for(int i=0 ;i< nums.size(); i++){
+            presum += nums[i];
+            int remove= presum-k;
+            count += mpp[remove];
+            mpp[presum] ++;
+        }
+
+        return count;
+    }
+};
