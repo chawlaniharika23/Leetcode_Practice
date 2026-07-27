04aeb7edb7908b523505fec69554328dcecae4fa
Niharika Chawla <chawlaniharika23@gmail.com>
2026-07-27 12:54:37 +0530

    Create 242_ValidAnagram.cpp

---
+++ Arrays/242_ValidAnagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()){
+            return false;
+        }
+
+        unordered_map <char, int> freq;
+        for (int i=0; i<s.length(); i++){
+            freq[s[i]]++;
+        }
+
+        for(int i=0; i<t.length(); i++){
+            freq[t[i]]--;
+
+            if (freq[t[i]]<0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
