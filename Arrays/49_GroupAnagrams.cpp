95e0e999b36343099f6241c00d414ae8b8c0dd23
Niharika Chawla <chawlaniharika23@gmail.com>
2026-08-03 15:20:12 +0530

    Update 49_GroupAnagrams.cpp

--- Arrays/49_GroupAnagrams.cpp
+++ Arrays/49_GroupAnagrams.cpp
@@ -1,12 +1,41 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map <string, vector<string>> mp;
-        for(string s : strs){
-            string temp=s;
-            sort(temp.begin(), temp.end());
+        // //Sorting 
+        // unordered_map <string, vector<string>> mp;
+        // for(string s : strs){
+        //     string temp=s;
+        //     sort(temp.begin(), temp.end());
 
-            mp[temp].push_back(s);
+        //     mp[temp].push_back(s);
+        // }
+
+        // vector<vector<string>> ans;
+
+        // for(auto it: mp){
+        //     ans.push_back(it.second);
+        // }
+
+        // return ans;
+
+        //Using a frequency key
+
+        unordered_map <string, vector<string>> mp;
+
+        for(string s : strs){
+            vector<int> freq(26,0);
+
+            for(char c: s){
+                freq[c-'a']++;
+            }
+
+            string key;
+
+            for(int x : freq){
+                key += to_string(x) + "#";
+            }
+
+            mp[key].push_back(s);
         }
 
         vector<vector<string>> ans;
