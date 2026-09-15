fec758cbaf407525cbea74d07c649a41c78e9f5a
Niharika Chawla <chawlaniharika23@gmail.com>
2026-09-15 23:36:49 +0530

    Update 28_FindTheIndexOfTheFirstOccurrenceInAString.cpp

--- Strings/28_FindTheIndexOfTheFirstOccurrenceInAString.cpp
+++ Strings/28_FindTheIndexOfTheFirstOccurrenceInAString.cpp
@@ -1,1 +1,30 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int len1= haystack.length();
+        int len2= needle.length();
 
+        // for(int i=0,;=0; i<len1, j<len2; i++,j++){
+        //     if(haystack[i] != needle[j]){
+        //         i++;
+        //     }
+        //     else if(haystack [i] == needle [j]){
+                
+        //     }
+        // }
+
+        for( int i=0; i<=len1-len2; i++){
+            int j=0;
+
+            while( j<len2 && haystack[i + j]==needle[j]){
+                j++;
+            }
+
+            if(j==len2){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
